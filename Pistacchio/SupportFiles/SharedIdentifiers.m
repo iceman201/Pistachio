c38a04bbcff516cc48611a0de554a8707118a19b
Liguo Jiao <liguo@jiao.co.nz>
2019-12-05 01:02:49 +1300

    Pistacchio

---
+++ Pistacchio/SupportFiles/SharedIdentifiers.m
@@ -0,0 +1,11 @@
+//
+//  SharedIdentifiers.m
+//  Pistacchio
+//
+//  Created by Liguo Jiao on 5/12/19.
+//  Copyright © 2019 Liguo Jiao. All rights reserved.
+//
+
+#import "SharedIdentifiers.h"
+
+NSString *kNewsCellIdentifier = @"0000010401802e";
