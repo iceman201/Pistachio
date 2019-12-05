04e0a269d9cc1124f3e0bfee9d1a24f4b635f036
Liguo Jiao <liguo@jiao.co.nz>
2019-12-05 15:39:21 +1300

    Create shared constants

--- Pistacchio/SupportFiles/SharedConstants.m
+++ Pistacchio/SupportFiles/SharedConstants.m
@@ -1,12 +1,11 @@
 //
-//  SharedIdentifiers.h
+//  SharedIdentifiers.m
 //  Pistacchio
 //
 //  Created by Liguo Jiao on 5/12/19.
 //  Copyright © 2019 Liguo Jiao. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
+#import "SharedConstants.h"
 
-
-extern NSString *kNewsCellIdentifier;
+NSString *const NEWS_COLLECTION_CELL_ID = @"0000010401802e";
